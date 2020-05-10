a9a8496be6aff83e3bccbd6ae0231f6293833c8a
Mazen Imad Jaber <Mazenimad.jaber@connect.qut.edu.au>
2020-05-11 02:37:17 +1000

    Add project files.

---
+++ Workshop7Ex3/Graph.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop7Ex3
+{
+    class Graph
+    {
+        private int N;
+        private int[,] graph;
+        private bool[] visited;
+        private static int INFINITY = int.MaxValue;
+        public Graph(int N)
+        {
+            graph = new int[N, N];
+            // initialise the matrix to INFINITY
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    graph[i, j] = INFINITY;
+                }
+                graph[i, i] = 0;
+            }
+
+            visited = new bool[N];
+
+        }
+
+        /// Destructor dealloctes the adjacency matrix
+        ~Graph()
+        {
+            graph = null;
+            visited = null;
+        }
+
+        /// Adds an undirected edge to the graph between the source and destination vertices.
+        public void addEdge(int src, int dst, int weight)
+        {
+            graph[src, dst] = weight;
+            graph[dst, src] = weight;
+        }
+
+
+        /// Produces a string representation of the graph.
+        public static void write(Graph graph)
+        {
+            
+        }
+
+
+
+        /// Performs initialisation for the DFS algorithm
+        public void dfsRecursive(int start)
+        {
+            
+        }
+
+        /// Private implementation of the recursive depth first search algorithm
+        private void dfsRec(int current)
+        {
+           
+        }
+
+        /// Implementation of the iterative depth first search algorithm
+        public void dfs(int current)
+        {
+            
+        }
+
+        /// Implementation of the iterative breadth first search algorithm
+        public void bfs(int current)
+        {
+        }
+    }
+}
---
+++ Workshop7Ex3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop7Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader sr = null;
+            try
+            {
+                // read the number of vertices and edges
+                sr = new System.IO.StreamReader(args[0]);
+                int numVertices, numEdges, src, dst, weight;
+                Graph graph = null;
+                while (!sr.EndOfStream)
+                {
+                    numVertices = Int32.Parse(sr.ReadLine().Trim());
+                    numEdges = Int32.Parse(sr.ReadLine().Trim());
+                    graph = new Graph(numVertices);
+
+                    for (int edge = 0; edge < numEdges; edge++)
+                    {
+                        String[] tmp = sr.ReadLine().Trim().Replace("\t", " ")
+                            .Split(new char[] { ' ' });
+                        src = Int32.Parse(tmp[0]);
+                        dst = Int32.Parse(tmp[1]);
+                        weight = Int32.Parse(tmp[2]);
+                        graph.addEdge(src, dst, weight);
+                    }
+
+                }
+                Graph.write(graph);
+              //add calls to the BFS & DFS algorithms
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: Could not find file");
+            }
+            finally
+            {
+                // close the file
+                sr.Close();
+                sr = null;
+                Console.ReadKey();
+            }
+
+        }
+    }
+    
+}
